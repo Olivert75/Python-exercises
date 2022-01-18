c83aeb3e2e0f758d560c8452a7da3ddcfaaf994c
Olivert75 <oliver.ton75@gmai.com>
2022-01-18 11:44:09 -0600

    Introduction exercises

---
+++ python_introduction_exercises.py
@@ -0,0 +1,8 @@
+#Create a hello world program. Create a text file named python_introduction_exercises.py and write a program that prints "Hello, World!" to the console. Run this program from the command line.
+#Inside of your hello world program, create a variable named greeting that contains the message that you will print to the console. Run your script interactively (with python -i python_introduction_exercises.py) and view the contents of the greeting variable.
+
+print ('Hello World!')
+
+greeting = 'Hello Innis, this very first exercise for python'
+
+print (greeting)
