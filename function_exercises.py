a507fd778cdac8c7f159bc4ae2ab09c520a063ff
Olivert75 <oliver.ton75@gmai.com>
2022-01-20 17:05:17 -0600

    finished

--- function_exercises.py
+++ function_exercises.py
@@ -1,21 +1,77 @@
 #Define a function named is_two. It should accept one input and return True if the passed input is either the number or the string 2, False otherwise.
 
+def is_two(num):
+    if x == 2 or x == "2"
+        return True
+    else:
+        return False
+
 #Define a function named is_vowel. It should return True if the passed string is a vowel, False otherwise.
+
+def is_vowel(string_type):
+    if string_type.lower() in ['a', 'e', 'i', 'o', 'u']
+         return True
+    else:
+        return False
 
 #Define a function named is_consonant. It should return True if the passed string is a consonant, False otherwise. Use your is_vowel function to accomplish this.
 
+def is_consonant(letter):
+    if is_vowel(letter):
+        return False
+    else:
+        return True
+
 #Define a function that accepts a string that is a word. The function should capitalize the first letter of the word if the word starts with a consonant.
+
+def cap_first_letter(word):
+    if is_consonant(word[0]) == True:
+        return word.capitalize()
+    else:
+        return word
 
 #Define a function named calculate_tip. It should accept a tip percentage (a number between 0 and 1) and the bill total, and return the amount to tip.
 
+def calculate_tip(tips_percentage, bill_total):
+    if tips_percentage >= 1:
+         tips_percentage /= 100
+    tips_amount = tips_percentage * bill_total
+    return tips_amount
+
 #Define a function named apply_discount. It should accept a original price, and a discount percentage, and return the price after the discount is applied.
+
+def apply_discount(original_price, discount_percent):
+    discount = original_price * discount_percent
+    return discount_percent - discount
 
 #Define a function named handle_commas. It should accept a string that is a number that contains commas in it as input, and return a number as output.
 
+def handle_commas(c):
+    return float(c.replace(',', ''))
+
 #Define a function named get_letter_grade. It should accept a number and return the letter grade associated with that number (A-F).
+
+def get_letter_grade(letter_grade):
+    if 90 <= letter_grade <= 100:
+        return "A"
+    elif 80 <= letter_grade <= 89:
+        return "B"
+    elif 70 <= letter_grade <= 79:
+        return "C"
+    elif 60 <= letter_grade <= 69:
+        return "D"
+    else:
+        return "F"
 
 #Define a function named remove_vowels that accepts a string and returns a string with all the vowels removed.
 
+def remove_vowels(word):
+    white_space = ''
+    for i in word:
+        if i in "aeiou":
+            white_space = word.replace(i, '')
+    return white_space
+ 
 #Define a function named normalize_name. It should accept a string and return a valid python identifier, that is:
 #anything that is not a valid python identifier should be removed
 #leading and trailing whitespace should be removed
@@ -25,6 +81,7 @@
 #Name will become name
 #First Name will become first_name
 # % Completed will become completed
+
 
 #Write a function named cumulative_sum that accepts a list of numbers and returns a list that is the cumulative sum of the numbers in the list.
 #cumulative_sum([1, 1, 1]) returns [1, 2, 3]
