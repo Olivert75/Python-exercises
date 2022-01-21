49a000e75d6506f8fa6e56555361ca30df59e32d
Olivert75 <oliver.ton75@gmai.com>
2022-01-20 23:49:29 -0600

    fix and add one problem

--- function_exercises.py
+++ function_exercises.py
@@ -1,5 +1,8 @@
 #Define a function named is_two. It should accept one input and return True if the passed input is either the number or the string 2, False otherwise.
 
+from functools import total_ordering
+
+
 def is_two(num):
     if x == 2 or x == "2"
         return True
@@ -8,12 +11,10 @@ def is_two(num):
 
 #Define a function named is_vowel. It should return True if the passed string is a vowel, False otherwise.
 
-def is_vowel(string_type):
-    if string_type.lower() in ['a', 'e', 'i', 'o', 'u']
-         return True
-    else:
-        return False
+def is_vowel(letter_type):
+    return letter_type.lower() in ['a', 'e', 'i', 'o', 'u']
 
+is_vowel("r")
 #Define a function named is_consonant. It should return True if the passed string is a consonant, False otherwise. Use your is_vowel function to accomplish this.
 
 def is_consonant(letter):
@@ -25,11 +26,12 @@ def is_consonant(letter):
 #Define a function that accepts a string that is a word. The function should capitalize the first letter of the word if the word starts with a consonant.
 
 def cap_first_letter(word):
-    if is_consonant(word[0]) == True:
+    if is_consonant(word[0]) == str:
         return word.capitalize()
     else:
         return word
 
+cap_first_letter("hddwyw")
 #Define a function named calculate_tip. It should accept a tip percentage (a number between 0 and 1) and the bill total, and return the amount to tip.
 
 def calculate_tip(tips_percentage, bill_total):
@@ -65,13 +67,12 @@ def get_letter_grade(letter_grade):
 
 #Define a function named remove_vowels that accepts a string and returns a string with all the vowels removed.
 
-def remove_vowels(word):
-    white_space = ''
-    for i in word:
+def remove_vowels(rm_vowels):
+    for i in rm_vowels:
         if i in "aeiou":
-            white_space = word.replace(i, '')
-    return white_space
- 
+            rm_vowels = rm_vowels.replace(i, '')
+    return rm_vowels
+
 #Define a function named normalize_name. It should accept a string and return a valid python identifier, that is:
 #anything that is not a valid python identifier should be removed
 #leading and trailing whitespace should be removed
@@ -82,14 +83,34 @@ def remove_vowels(word):
 #First Name will become first_name
 # % Completed will become completed
 
+def normalize_name(rm_char):
+    result = ''
 
+    rm_char = rm_char.lower()
+
+    for i in rm_char:
+        if i.isidentifier() or i == " ":
+            result += i
+    
+    result = result.replace(" ", "_")
+    result = result.strip()
+    
+    return result
+
+normalize_name("First Name")
 #Write a function named cumulative_sum that accepts a list of numbers and returns a list that is the cumulative sum of the numbers in the list.
 #cumulative_sum([1, 1, 1]) returns [1, 2, 3]
 #cumulative_sum([1, 2, 3, 4]) returns [1, 3, 6, 10]
 
+def cumulative_sum(lst):
+    for i in range (1, len(lst)):
+        lst[i] += sum(lst[i -1:i])
+    return lst
+
 #Additional Functions Exercise
 
-#Comment each line of your function with an explanation of what that line is doing. Make sure to document both the type of all function parameters and the type that will be returned from the function. Also be sure to use the vocabulary we've introduced to describe your python code appropriately.
+#Comment each line of your function with an explanation of what that line is doing. Make sure to document both the type of all function parameters and the type that will be returned from the function. 
+#Also be sure to use the vocabulary we've introduced to describe your python code appropriately.
 #Walk us through how the function executes using the comments you wrote and several different example inputs that demonstrate all the possible paths through your function.
 
 #When we pass "Darden", the conditional will evaluate to True and the first branch of the if statement will be followed. 
